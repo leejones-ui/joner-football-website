fda6efe0ecbffa6976233532568dd4f6d309e63e
Joner Ai <jonerai@Mac-3.modem>
2026-05-11 15:20:37 +1000

    Rebuild JFP page using about style

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,10 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
-import react from '@astrojs/react';
 
 export default defineConfig({
   site: 'https://jonerfootball.com',
-  integrations: [react(), sitemap({
+  integrations: [sitemap({
     filter: (page) => {
       const excludedPaths = [
         '/checkout-success/',
